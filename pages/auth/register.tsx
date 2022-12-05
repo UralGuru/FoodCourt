b7b172e15ffe284a72ff5b43a08e47c06dd9ce20
Ural <gizzatullinu@gmail.com>
2022-12-05 15:33:35 +0500

    RegisterPage/Layout

--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -1,216 +1,170 @@
-// import {FC} from 'react';
-// import React, {useState, useEffect} from "react";
-// import {Formik, Field, Form, ErrorMessage} from "formik";
-// import * as Yup from "yup";
-// import {AiOutlineGoogle} from 'react-icons/ai';
-// import {BsEye, BsEyeSlash} from 'react-icons/bs';
-//
-// // import { useDispatch, useSelector } from "react-redux";
-// // import { login } from "../slices/authSlice";
-// // import { clearMessage } from "../slices/messageSlice";
-// import styles from "./auth.module.scss"
-// import {registerType} from "../../constants/types";
-// import {useRouter} from "next/router";
-// import {useDispatch, useSelector} from "react-redux";
-// import {clearMessage, register} from "../../store/slices/authSlice";
-// import {log} from "util";
-//
-// export default function RegisterPage() {
-//     const router = useRouter();
-//     const dispatch = useDispatch();
-//     const user = useSelector((state: any) => state.auth);
-//
-//     const [loading, setLoading] = useState(false);
-//     const [pswView, setPswView] = useState(false);
-//
-//     useEffect(() => {
-//         // @ts-ignore
-//         dispatch(clearMessage());
-//     }, [dispatch]);
-//
-//     const initialValues: registerType = {
-//         name: "",
-//         email: "",
-//         phone: "",
-//         password: "",
-//     };
-//
-//     const validationSchema = Yup.object().shape({
-//         name: Yup.string().required("Введите имя"),
-//         phone: Yup.string().required("Введите телефон"),
-//         email: Yup.string().email("Некорректный ввод").required("Введите логин"),
-//         password: Yup.string().required("Придумайте пароль"),
-//     });
-//
-//     const handleLogin = (formValue: registerType) => {
-//         setLoading(true);
-//         // @ts-ignore
-//         dispatch(register(formValue))
-//             .unwrap()
-//             // .then(() => {
-//             //     navigate("/profile");
-//             // })
-//             .catch(() => {
-//                 setLoading(false);
-//             });
-//     };
-//     // if (isLoggedIn) {
-//     //     return <Navigate to="/profile" />;
-//     // }
-//
-//     if (user.isLoggedIn) {
-//         return router.push('/')
-//     }
-//
-//
-//     return (
-//         <>
-//             <div className={styles.header}>Регистрация</div>
-//             <div className={styles.pageContent}>
-//
-//                 <div className={styles.auth}>
-//                     <Formik
-//                         initialValues={initialValues}
-//                         validationSchema={validationSchema}
-//                         onSubmit={handleLogin}
-//                     >
-//                         {({errors, touched}) => (
-//                             <Form>
-//
-//                                 {/* NAME */}
-//                                 <div className={styles.label}>
-//                                     <label htmlFor="name" className={styles.labelText}>Имя</label>
-//                                     <div>
-//                                         <Field
-//                                             id="name"
-//                                             name="name"
-//                                             type="text"
-//                                             className={
-//                                                 styles.labelInput +
-//                                                 (errors.email && touched.email ? " is-invalid" : "")
-//                                             }
-//                                         />
-//                                     </div>
-//                                     <div className={styles.errorMsg}><ErrorMessage name="name" component="div"/></div>
-//                                 </div>
-//
-//                                 {/* PHONE */}
-//                                 <div className={styles.label}>
-//                                     <label htmlFor="phone" className={styles.labelText}>Телефон</label>
-//                                     <div>
-//                                         <Field
-//                                             id="phone"
-//                                             name="phone"
-//                                             type="text"
-//                                             className={
-//                                                 styles.labelInput +
-//                                                 (errors.email && touched.email ? " is-invalid" : "")
-//                                             }
-//                                         />
-//                                     </div>
-//                                     <div className={styles.errorMsg}><ErrorMessage name="phone" component="div"/></div>
-//                                 </div>
-//
-//                                 {/* LOGIN */}
-//                                 <div className={styles.label}>
-//                                     <label htmlFor="email" className={styles.labelText}>Логин</label>
-//                                     <div>
-//                                         <Field
-//                                             id="email"
-//                                             name="email"
-//                                             type="text"
-//                                             className={
-//                                                 styles.labelInput +
-//                                                 (errors.email && touched.email ? " is-invalid" : "")
-//                                             }
-//                                         />
-//                                     </div>
-//
-//                                     <div className={styles.errorMsg}><ErrorMessage name="email" component="div"/></div>
-//
-//                                 </div>
-//
-//                                 {/* PASSWORD */}
-//                                 <div className={styles.label}>
-//                                     <label htmlFor="password" className={styles.labelText}>Пароль</label>
-//                                     <div>
-//                                         <Field
-//                                             id="password"
-//                                             name="password"
-//                                             type={pswView ? "text" : "password"}
-//                                             autoComplete="on"
-//
-//                                             className={
-//                                                 styles.labelInput +
-//                                                 (errors.password && touched.password ? " is-invalid" : "")
-//                                             }
-//                                         />
-//                                         <span className={styles.eyeContainer} onClick={() => setPswView(!pswView)}>
-//                                     <span className={styles.eye}>
-//                                         {pswView
-//                                             ? <BsEye style={{fontSize: 20}}/>
-//                                             : <BsEyeSlash style={{fontSize: 20}}/>}
-//                                     </span>
-//
-//                                 </span>
-//
-//                                     </div>
-//
-//                                     <div className={styles.errorMsg}><ErrorMessage name="password" component="div"/></div>
-//                                 </div>
-//
-//
-//                                 {user.message && (
-//                                     <div className={styles.errorBlock}>
-//                                         <div className={styles.errorMsg}>
-//                                             {user.message === 'User did not created'
-//                                                 ? "Пользователь не создан"
-//                                                 : user.isSuccess
-//                                                     ? "Успешная регистрация"
-//                                                     : user.message
-//                                             }
-//
-//
-//                                         </div>
-//                                     </div>
-//                                 )}
-//
-//                                 {/* SUBMIT */}
-//                                 <div>
-//                                     <button
-//                                         type="submit"
-//                                         className={styles.button}
-//                                         // disabled={loading}
-//                                     >
-//                                         {loading && (<span className="spinner-border spinner-border-sm"></span>)}
-//                                         <div>Зарегистрироваться</div>
-//                                     </button>
-//                                 </div>
-//
-//
-//                             </Form>
-//                         )}
-//                     </Formik>
-//
-//                     <div className={styles.textGroupBetweenBtn}>
-//                         <div className={styles.textBetweenBtn}>Или зарегистрируйтесь с помощью</div>
-//                     </div>
-//
-//
-//                     {/* SUBMIT WIDTH GOOFLE*/}
-//                     <div>
-//                         <button
-//                             className={`${styles.button} ${styles.buttonGoogle}`}
-//                             // disabled={loading}
-//                             onClick={() => console.log('Google')}
-//                         >
-//                             <AiOutlineGoogle style={{fontSize: 25, marginRight: 5}}/>
-//                             <div>Google</div>
-//                         </button>
-//                     </div>
-//                 </div>
-//             </div>
-//         </>
-//     )
-//         ;
-// };
+import React, { useState, useEffect } from 'react';
+import cn from 'classnames'
+import { AiOutlineGoogle } from 'react-icons/ai';
+import { BsEye, BsEyeSlash } from 'react-icons/bs';
+import styles from './auth.module.scss';
+import { useForm } from "react-hook-form";
+
+export default function RegisterPage() {
+    // const router = useRouter();
+    // const dispatch = useDispatch();
+    // const user = useSelector((state: any) => state.auth);
+    //
+    // const [loading, setLoading] = useState(false);
+    const [pswView, setPswView] = useState(false);
+
+    // useEffect(() => {
+    //     // @ts-ignore
+    //     dispatch(clearMessage());
+    // }, [dispatch]);
+
+    const initialValues: any = {
+        name: "",
+        email: "",
+        phone: "",
+        password: "",
+    };
+
+    const handleLogin = (formValue: any) => {
+        console.log(formValue)
+    };
+    // if (isLoggedIn) {
+    //     return <Navigate to="/profile" />;
+    // }
+
+    // if (user.isLoggedIn) {
+    //     return router.push('/')
+    // }
+
+    const {register, formState: {errors}, handleSubmit, control} = useForm();
+    const passwordShowEyeIcon = pswView ? <BsEye style={{fontSize: 20}}/> : <BsEyeSlash style={{fontSize: 20}}/>;
+    const typePassword = pswView ? 'text' : 'password';
+
+    return (
+        <React.Fragment>
+            <div className={styles.header}>РЕГИСТРАЦИЯ</div>
+            <div className={styles.pageContent}>
+                <div className={styles.auth}>
+                    <form onSubmit={handleSubmit(handleLogin)}>
+
+                        <div className={styles.label}>
+                            <label className={styles.labelText} htmlFor="name">Имя</label>
+                            <div>
+                                <input
+                                    id="name" className={cn(styles.labelInput)}
+                                    {...register("name", {
+                                        required: "name",
+                                    })}
+                                    type='text'
+                                />
+                                {errors.name &&
+                                    <div><span className={styles.errorMsg} role="alert">Введите имя</span>
+                                    </div>}
+                            </div>
+                        </div>
+
+                        <div className={styles.label}>
+                            <label className={styles.labelText} htmlFor="phone">Телефон</label>
+                            <div>
+                                <input
+                                    id="phone" className={cn(styles.labelInput)}
+                                    {...register("phone", {
+                                        required: "phone",
+                                    })}
+                                    type='text'
+                                />
+                                {errors.name &&
+                                    <div><span className={styles.errorMsg} role="alert">Введите имя</span>
+                                    </div>}
+                            </div>
+                        </div>
+
+                        <div className={styles.label}>
+                            <label className={styles.labelText} htmlFor="email">Почта</label>
+                            <div>
+                                <input id="email" className={cn(styles.labelInput)}
+                                       {...register("email", {
+                                           required: "required",
+                                           pattern: {
+                                               value: /\S+@\S+\.\S+/,
+                                               message: ''
+                                           }
+                                       })} type="email"/>
+                                {errors.email &&
+                                    <div><span className={styles.errorMsg} role="alert">Введите логин</span></div>}
+                            </div>
+                        </div>
+
+                        <div className={styles.label}>
+                            <label className={styles.labelText} htmlFor="password">Пароль</label>
+                            <div>
+                                <input
+                                    id="password" className={cn(styles.labelInput)}
+                                    {...register("password", {
+                                        required: "required",
+                                        minLength: {
+                                            value: 5,
+                                            message: ""
+                                        }
+                                    })}
+                                    type={typePassword}
+                                />
+                                <span className={styles.eyeContainer} onClick={() => setPswView(!pswView)}>
+                                    <span className={styles.eye}>
+                                        {passwordShowEyeIcon}
+                                    </span>
+                                </span>
+                                {errors.password &&
+                                    <div><span className={styles.errorMsg} role="alert">Пароль должен содержать не менее 5 символов</span>
+                                    </div>}
+                            </div>
+                        </div>
+
+                        {/* SUBMIT */}
+                        <div>
+                            <button
+                                type='submit'
+                                className={styles.button}
+                            >
+                                <div>Войти</div>
+                            </button>
+                        </div>
+                    </form>
+
+                    {/*{user.message && (*/}
+                    {/*    <div className={s.errorBlock}>*/}
+                    {/*        <div className={s.errorMsg}>*/}
+                    {/*            {user.message === 'User did not created'*/}
+                    {/*                ? "Пользователь не создан"*/}
+                    {/*                : user.isSuccess*/}
+                    {/*                    ? "Успешная регистрация"*/}
+                    {/*                    : user.message*/}
+                    {/*            }*/}
+
+
+                    {/*        </div>*/}
+                    {/*    </div>*/}
+                    {/*)}*/}
+
+
+                    {/*SUBMIT WIDTH GOOFLE*/}
+                    <div className={styles.textGroupBetweenBtn}>
+                        <div className={styles.textBetweenBtn}>Или войдите с помощью</div>
+                    </div>
+                    <div>
+                        <button
+                            className={cn(styles.button, styles.buttonGoogle)}
+                            // disabled={loading}
+                            // onClick={() => AuthService.loginWidthGoogle()}
+                            onClick={() => console.log('Auth width Google')}
+                        >
+                            <AiOutlineGoogle style={{fontSize: 25, marginRight: 5}}/>
+                            <div>Google</div>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>
+    )
+        ;
+};
