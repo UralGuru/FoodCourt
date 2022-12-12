549584bf8cb1845dd320c658c1b7558e15bf4d3f
Ural <gizzatullinu@gmail.com>
2022-12-12 12:18:53 +0500

    CafeCardCreates

---
+++ components/cards/cafeCard/cafeCard.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {AiFillStar} from "react-icons/ai";
+import styles from "./cafeCard.module.scss";
+
+interface CafeCard {
+    id: number,
+    name: string,
+    distance: string,
+    avatar: string,
+    adress: string,
+}
+
+export default function CafeCard(props: CafeCard) {
+    const rating = (Math.floor(Math.random() * (50 - 30 + 1) + 30)/10).toFixed(1)
+
+    return <div className={styles.cafeList}>
+        <div suppressHydrationWarning>Ignore this</div>
+        <div className={styles.cafeItem} style={{backgroundImage: `url(${props.avatar})`}}>
+            <div className={styles.cafeName}>{props.name}</div>
+            <div className={styles.cafeDescription}>
+                <div>{props.distance}</div>
+                <div className={styles.rating}>
+                    <AiFillStar className={styles.ratingStar}/>
+                    {rating}
+                </div>
+            </div>
+        </div>
+    </div>
+}
--- pages-content/home/index.tsx
+++ pages-content/home/index.tsx
@@ -5,40 +5,32 @@ import {useDispatch, useSelector} from 'react-redux';
 import {AiFillStar, AiOutlineSearch} from "react-icons/ai";
 import {clearMessage} from '@store/slices/authSlice';
 import {UrlManager} from '@shared/urls';
+import CafeCard from "@components/cards/cafeCard/cafeCard";
 import styles from './home.module.scss';
 
 
 const cafes = {
     foundEntities: [
         {
-            id: 1,
-            name: "Хас кебаб",
-            description: "Норм вкусно супер хадляль",
-            status: "Rejected",
-            avatar: "sadas",
-            adress: "малышева 121",
-            latitude: 0,
-            longitude: 0
+            id: 3,
+            name: "кулинария",
+            distance: "117 м",
+            avatar: "https://avatars.mds.yandex.net/get-altay/4562252/2a0000017b79e05823ae8f3110fa98516684/XXL",
+            adress: "комсомольская 72"
         },
         {
             id: 2,
-            name: "Ян 2",
-            description: "что-то",
-            status: "Created",
-            avatar: "",
-            adress: "малышева 127",
-            latitude: 0,
-            longitude: 0
+            name: "вилка ложка",
+            distance: "1,8 км",
+            avatar: "https://spbu.ru/sites/default/files/losh0638.jpg",
+            adress: "Ленина 127"
         },
         {
-            id: 5,
-            name: "что-то2",
-            description: "что-то2",
-            status: "Created",
-            avatar: "",
-            adress: "малышева 127",
-            latitude: 0,
-            longitude: 0
+            id: 1,
+            name: "KFC",
+            distance: "3,07 км",
+            avatar: "https://icdn.lenta.ru/images/2022/12/07/15/20221207150941564/square_1024_webp_87bd85a5e1e7217c424bbe647e5d1d42.webp",
+            adress: "Ленина 127"
         }
     ],
     totalCount: 3
@@ -69,19 +61,9 @@ export const HomePageContent: FC = () => {
                     <AiOutlineSearch className={styles.searchIcon}/>
                     Поиск
                 </div>
-                <div className={styles.cafeList}>
-                    <div className={styles.cafeItem}>
-                        <div className={styles.cafeName}>Бургер Кинг</div>
-                        <div className={styles.cafeDescription}>
-                            <div>500 m</div>
-                            <div className={styles.rating}>
-                                    <AiFillStar className={styles.ratingStar}/>
-                                5.0
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div></div>
+
+                {cafes.foundEntities.map(cafe => <CafeCard key={cafe.id} id={cafe.id} name={cafe.name} distance={cafe.distance} avatar={cafe.avatar} adress={cafe.adress} />)}
+
             </div>
         </React.Fragment>
     );
