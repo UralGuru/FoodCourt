7d418d6185fdbf6d32ee23d8dc7e1349995e075a
Ural <gizzatullinu@gmail.com>
2022-12-15 16:28:57 +0500

    BasketOrderPage

--- pages-content/basket/order/order.tsx
+++ pages-content/basket/order/order.tsx
@@ -17,7 +17,6 @@ export function Order() {
                 <div className={styles.headerTitle}>Оформление заказа</div>
                 <div></div>
             </div>
-
             <div className={styles.result}>
                 <div className={styles.title}>Товары</div>
                 <div>
@@ -25,14 +24,22 @@ export function Order() {
                         <div>ANGUS</div>
                         <div>800 ₽</div>
                     </div>
+                    <div className={styles.product}>
+                        <div>Сухари</div>
+                        <div>100 ₽</div>
+                    </div>
+                    <div className={styles.product}>
+                        <div>Запеченая курица</div>
+                        <div>800 ₽</div>
+                    </div>
+                    <div className={styles.resultPrice}>
+                        <div>Итого</div>
+                        <div>1700 ₽</div>
+                    </div>
                 </div>
             </div>
-
         </div>
-
-
         <div className={cn(styles.button, styles.submitButton)}>Заказать</div>
-
         <div className={styles.placeForButtons}/>
     </React.Fragment>
 }
