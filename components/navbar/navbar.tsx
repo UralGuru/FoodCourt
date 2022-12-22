0232a3596c33a7618ce08ecf18bc70c51c3e5de5
Ural <gizzatullinu@gmail.com>
2022-12-22 15:33:33 +0500

    NavbarAddIcoms

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -3,6 +3,9 @@ import React from "react";
 import {useRouter} from "next/router";
 import {URLManager} from "@shared/url-manager";
 import styles from "./navbar.module.scss";
+import {MdHistory, MdHome} from "react-icons/md";
+import {FaShoppingBasket} from "react-icons/fa";
+import {IoMdSettings} from "react-icons/io";
 
 export const Navbar = () => {
     const router = useRouter();
@@ -21,10 +24,22 @@ export const Navbar = () => {
 
     return (
         <div className={styles.navbar}>
-            <div className={cn(styles.navbarItem, activeHomePage)} onClick={LinkHomePage}>Home</div>
-            <div className={cn(styles.navbarItem, activeBasketPage)} onClick={LinkBasketPage}>Basket</div>
-            <div className={cn(styles.navbarItem, activeSettingsPage)} onClick={LinkSettingsPage}>Settings</div>
-            <div className={cn(styles.navbarItem, activeHistoryPage)} onClick={LinkHistoryPage}>History</div>
+            <div className={cn(styles.navbarItem, activeHomePage)} onClick={LinkHomePage}>
+                <MdHome className={styles.icon}/>
+                <div>Главная</div>
+            </div>
+            <div className={cn(styles.navbarItem, activeBasketPage)} onClick={LinkBasketPage}>
+                <FaShoppingBasket className={styles.icon} />
+                <div>Корзина</div>
+            </div>
+            <div className={cn(styles.navbarItem, activeSettingsPage)} onClick={LinkSettingsPage}>
+                <IoMdSettings className={styles.icon} />
+                <div>Настройки</div>
+            </div>
+            <div className={cn(styles.navbarItem, activeHistoryPage)} onClick={LinkHistoryPage}>
+                <MdHistory className={styles.icon} />
+                <div>История</div>
+            </div>
         </div>
     )
 }
