facbe95266ab8f31e67242740141e74d5e004c63
Ural <gizzatullinu@gmail.com>
2022-12-24 08:46:02 +0500

    GoogleLogin

--- shared/HOC.tsx
+++ shared/HOC.tsx
@@ -1,20 +1,29 @@
 import {useRouter} from "next/router";
 import React, {FC, useEffect, useState} from "react";
 import {URLManager} from "@shared/url-manager";
+import {useCookies} from "react-cookie";
 
 export const withAuth = (Component: FC) => {
     const AuthenticatedComponent = () => {
+        const [cookie, setCookie] = useCookies(['ApiTokens'])
         const router = useRouter();
         const [data, setData] = useState(false);
+        console.log(cookie)
 
 
         useEffect(() => {
             const getUser = async () => {
                 let accessToken = '';
                 if (typeof window !== 'undefined') {
+                    console.log(cookie)
+                    cookie && localStorage.setItem('access-token', cookie.ApiTokens)
                     accessToken = localStorage.getItem('access-token') ?? "";
+
+
+
                 }
                 if (!accessToken) {
+                    localStorage.setItem('access-token', cookie.ApiTokens)
                     router.push(URLManager.getLoginURL());
                 } else {
                     setData(!!accessToken);
